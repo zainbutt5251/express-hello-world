186644fb9c6b66ff179c4e03920b011a1a57e70a
zainbutt5251 <zainbutt5251@gmail.com>
2022-11-07 00:09:42 +0500

    final1

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -6,35 +6,38 @@ exports.homepage = (req, res) => {
     res.render('index.ejs');
 };
 exports.login = (req, res) => {
+    console.log("i,m in login page")
     res.render('login.ejs');
 };
 
 
 exports.Dashboard = (req, res) => {
+    console.log("i,m in login dashboard")
+
     res.render("Dashboard.ejs");
 };
 
-exports.Blogs =  async  (req, res)=> {
+exports.Blogs = async (req, res) => {
     res.render("Blog.ejs")
 }
-exports.getBlogs =  async  (req, res)=> {
+exports.getBlogs = async (req, res) => {
     Blog.find().lean()
-   .exec((err, items)=>{
-        if(!err){
-            return res.json({items: items})
-        }
-    })
+        .exec((err, items) => {
+            if (!err) {
+                return res.json({ items: items })
+            }
+        })
 }
 
 
-exports.Article =  async  (req, res)=> {
+exports.Article = async (req, res) => {
     res.render("Article.ejs")
 }
-exports.getArticle =  async  (req, res)=> {
+exports.getArticle = async (req, res) => {
     Article.find().lean()
-   .exec((err, items)=>{
-        if(!err){
-            return res.json({items: items})
-        }
-    })
+        .exec((err, items) => {
+            if (!err) {
+                return res.json({ items: items })
+            }
+        })
 }
