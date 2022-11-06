6ea39afe7917c9ea59eea630ef6299b622af1b2d
zainbutt5251 <zainbutt5251@gmail.com>
2022-11-07 00:04:14 +0500

    final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const DB = require("./db");
+const path = require('path')
 var cors = require('cors');
 var bodyparser = require("body-parser")
 const bcrypt = require('bcrypt');
@@ -9,11 +10,12 @@ const app = express()
 
 // #############################################################################
 // Logs all request paths and method
-// 
+
 const AppRoutes = require("./src/routes/router")
 app.use(AppRoutes);
 app.use(express.json());
 app.set('view engine', 'ejs');
+app.set('views', path.join('src/views'));
 app.use(cors()) // Use this after the variable declaration
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -66,33 +68,5 @@ app.post("/register", async (req, res) => {
         res.status(200).json(err);
     }
 });
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('src/views', options))
-
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
-})
 
 module.exports = app
